ee0fa6e5795713b8e5c3ad5ecb7dd1399a4ec816
Matvey Glushko <matveyka_glushko@mail.ru>
2023-07-06 08:59:02 +0300

    not working

--- src/strings/addBinary.ts
+++ src/strings/addBinary.ts
@@ -4,14 +4,14 @@ let b: string = "1011"
 function addBinary(a: string, b: string): string {
     let ans = "";
     let ca = 0;
-    for(let i = a.length - 1, j = b.length - 1;i >= 0 || j >= 0; i--, j--) {
-        ca += i >= 0 ? parseInt(a[i]) : 0;
-        ca += j >= 0 ? parseInt(b[j]) : 0;
+    for(let i = 0, j = 0; i < a.length || j < b.length; i++, j++) {
+        ca += i < a.length ? parseInt(a[i]) : 0;
+        ca += j < b.length ? parseInt(b[j]) : 0;
         ans += ca % 2;
         ca = Math.floor(ca / 2);
     }
     ans += ca == 1 ? ca : "";
-    return ans.split('').reverse().join('');
+    return ans;
 }
 
 console.log(addBinary(a,b))
