4e8de4d3b66e71131f3cbe90ec5b7c2db9dca892
Matvey Glushko <matveyka_glushko@mail.ru>
2023-07-06 09:15:56 +0300

    crushed

--- src/strings/addBinary.ts
+++ src/strings/addBinary.ts
@@ -1,7 +1,7 @@
 function addBinary(a: string, b: string): string {
     let ans = "";
     let ca = 0;
-    for(let i = a.length - 1, j = b.length - 1;i >= 0 || j >= 0; i--, j--) {
+    for(let i = a.length - 1, j = b.length - 1;i > 0 || j >= 0; i--, j--) {
         ca += i >= 0 ? parseInt(a[i]) : 0;
         ca += j >= 0 ? parseInt(b[j]) : 0;
         ans += ca % 2;
