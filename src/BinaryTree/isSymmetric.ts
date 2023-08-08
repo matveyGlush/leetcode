0ece5b9507e6274cc8eae427f874743711ef071a
Matvey Glushko <matveyka_glushko@mail.ru>
2023-08-08 15:11:56 +0300

    Symmetric Tree

---
+++ src/BinaryTree/isSymmetric.ts
@@ -0,0 +1,11 @@
+function isSymmetric(root: TreeNode | null): boolean {
+
+  const isMirror = (t1: TreeNode | null, t2: TreeNode | null): boolean => {
+    if (t1 === null && t2 === null) return true;
+    if (t1 === null || t2 === null) return false;
+
+    return (t1.val === t2.val) && isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+  }
+
+  return isMirror(root, root);
+}
